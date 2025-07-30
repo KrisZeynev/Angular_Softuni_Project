775c3b2849c6ddb02d20d6885bde76d89be689fd
Kris Zeynev <kriszeynev@yahoo.com>
2025-07-30 22:37:13 +0300

    add about page content and implements its styles

--- books-project/src/app/app.ts
+++ books-project/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { AboutPage } from './pages/about-page/about-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePage, Header, Footer],
+  imports: [RouterOutlet, HomePage, Header, Footer, AboutPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
