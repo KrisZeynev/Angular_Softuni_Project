eeb772736f43f03d2b7efd50e5a7f7f78c699833
Kris Zeynev <kriszeynev@yahoo.com>
2025-07-30 16:30:15 +0300

    add style to the header component

--- books-project/src/app/app.ts
+++ books-project/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePage],
+  imports: [RouterOutlet, HomePage, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
