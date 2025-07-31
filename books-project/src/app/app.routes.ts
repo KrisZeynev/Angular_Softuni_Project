4298fced07c192748040be309fb6d0c50999f8d2
Kris Zeynev <kriszeynev@yahoo.com>
2025-07-31 23:40:56 +0300

    create preferences page

--- books-project/src/app/app.routes.ts
+++ books-project/src/app/app.routes.ts
@@ -3,6 +3,20 @@ import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  //   { path: 'home', loadComponent: () =>('').then(c=> c.home) },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home-page/home-page').then((c) => c.HomePage),
+  },
+  {
+    path: 'catalog',
+    loadComponent: () =>
+      import('./pages/catalog-page/catalog-page').then((c) => c.CatalogPage),
+  },
+//   {
+//     path: 'preferences',
+//     loadComponent: () =>
+//       import('./pages/'),
+//   },
   { path: '**', component: NotFound },
 ];
---
+++ books-project/src/app/pages/preferences-page/preferences-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PreferencesPage } from './preferences-page';
+
+describe('PreferencesPage', () => {
+  let component: PreferencesPage;
+  let fixture: ComponentFixture<PreferencesPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PreferencesPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PreferencesPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
