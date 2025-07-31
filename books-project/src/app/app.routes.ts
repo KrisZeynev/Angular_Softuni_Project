b71a130e5d4e4a8d9dc1d405a2d55801cad9b5f1
Kris Zeynev <kriszeynev@yahoo.com>
2025-07-31 23:37:04 +0300

    create not-found component

--- books-project/src/app/app.routes.ts
+++ books-project/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //   { path: 'home', loadComponent: () =>('').then(c=> c.home) },
+  { path: '**', component: NotFound },
+];
