ba204cf208e6aa0017ce1d9b9422987e7c0dd75a
Kris Zeynev <kriszeynev@yahoo.com>
2025-07-31 23:23:29 +0300

    implement auth guard

---
+++ books-project/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+
+
+import { inject } from "@angular/core"
+import { CanActivateFn, Router } from "@angular/router"
+// import { AuthService } from "../services"
+
+export const authGuard: CanActivateFn = (route, state) => {
+    // const authService = inject(AuthService);
+    const router = inject(Router);
+
+    const isLoggedInV = true;
+
+    // if (authService.isLoggedIn()) {
+    if (isLoggedInV) {
+        return true;
+    } else {
+        return router.createUrlTree(['/login'])
+    }
+}
