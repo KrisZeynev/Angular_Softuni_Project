0ba0354855356cbb457283615f1922033a778bf1
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-03 22:57:59 +0300

    add new route - get user by id

--- Rest-api/router/users.js
+++ Rest-api/router/users.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { authController } = require('../controllers');
 const { auth } = require('../utils');
+const User = require('../models/userModel');
 
 router.get('/profile', auth(),authController.getProfileInfo);
 router.put('/profile', auth(),authController.editProfileInfo);
+
+// get user by id
+router.get('/id/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        console.error('Error fetching user by ID:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
 
 module.exports = router
