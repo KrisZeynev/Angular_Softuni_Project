5221379419492fc76880ed44f6eeb6c7b8d81ab7
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-03 16:21:53 +0300

    update profile-page fields

--- books-project/src/app/pages/profile-page/profile-page.ts
+++ books-project/src/app/pages/profile-page/profile-page.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './profile-page.html',
-  styleUrl: './profile-page.css'
+  styleUrl: './profile-page.css',
 })
 export class ProfilePage {
+  private authService = inject(AuthService);
 
+  formData = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    profileImg: '',
+  };
+
+  ngOnInit(): void {
+    const user = this.authService.currentUser();
+    console.log(user);
+    if (user) {
+      this.formData.email = user.email || '';
+      this.formData.profileImg = user.profileImg || '';
+      this.formData.username = user.username || '';
+    }
+  }
+
+  onSubmit(): void {
+    console.log('Submitted formData:', this.formData);
+  }
 }
