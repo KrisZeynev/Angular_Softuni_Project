760ead96e6762475edeb23d0bb2db87a951bf505
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-05 00:17:49 +0300

    load actual user details

--- books-project/src/app/pages/profile-page/profile-page.ts
+++ books-project/src/app/pages/profile-page/profile-page.ts
@@ -3,39 +3,64 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 import { UserService } from '../../core/services/user.service';
 import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.css',
 })
 export class ProfilePage implements OnInit {
   private authService = inject(AuthService);
   private userService = inject(UserService);
-
   formData = {
-    username: '',
-    email: '',
-    profileImg: '',
+    username: localStorage.getItem('username') || '',
+    email: localStorage.getItem('email') || '',
+    profileImg: localStorage.getItem('profileImg') || '',
   };
+
+  constructor(private router: Router, private http: HttpClient) {}
+  
 
   errorMessage: string = '';
 
   ngOnInit(): void {
-    this.authService.loadCurrentUserFromAPI();
-
-    this.authService.currentUser$.subscribe((user) => {
-      if (user) {
-        console.log('Got user in ProfilePage:', user);
-        this.formData.email = user.email;
-        this.formData.profileImg = user.profileImg || '';
-        this.formData.username = user.username || '';
-      }
+    this.authService.currentUser().subscribe({
+      next: (user) => {
+        console.log('User:', user.username);
+        console.log('Email:', user.email);
+        console.log('Picture:', user.profileImg);
+        if (user) {
+          console.log('ajde');
+          this.formData.email = user.email || '';
+          this.formData.profileImg = user.profileImg || '';
+          this.formData.username = user.username || '';
+        }
+      },
+      error: (err) => {
+        console.error('Failed to fetch user:', err);
+      },
     });
+
+    // this.authService.loadCurrentUserFromAPI();
+
+    // this.authService.currentUser$.subscribe((user) => {
+    //   if (user) {
+    //     console.log('Got user in ProfilePage:', user);
+    //     this.formData.email = user.email;
+    //     this.formData.profileImg = user.profileImg || '';
+    //     this.formData.username = user.username || '';
+    //   }
+    // });
   }
+
+    
+  // }
 
   onSubmit(form: NgForm): void {
     this.errorMessage = '';
@@ -46,12 +71,46 @@ export class ProfilePage implements OnInit {
 
     const userId = this.authService.getUser('userId');
     console.log(`userId on profile page: ${userId}`);
-    
 
-    const updatedUser = {
+    const updatedUserData = {
       email: this.formData.email,
       username: this.formData.username,
       profileImg: this.formData.profileImg,
     };
+
+    const token = this.authService.getUser('accessToken') || ''; // примерно така взимаш токена
+    const headers = new HttpHeaders({
+      'X-Authorization': token,
+      'Content-Type': 'application/json',
+    });
+
+    this.http
+      .patch(`http://localhost:3030/users/${userId}`, {"username": this.formData.username}, {
+        headers,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log('User updated successfully', response);
+          localStorage.setItem('email', this.formData.email)
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          console.error('Update failed', error);
+        },
+      });
+
+    // Content-Type: application/json
+    // this.http
+    //   .patch(`http://localhost:3030/users/${userId}`, updatedUserData)
+    //   .subscribe({
+    //     next: (response) => {
+    //       console.log('User updated successfully', response);
+    //       this.authService.saveUser(response);
+    //       this.router.navigate(['/home']);
+    //     },
+    //     error: (error) => {
+    //       console.error('Update failed', error);
+    //     },
+    //   });
   }
 }
