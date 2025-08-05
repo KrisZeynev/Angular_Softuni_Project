f3a329fb18db51caf0bb30ccfb39b4b8263e1051
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-05 16:15:45 +0300

    implement BookService to post new book

--- books-project/src/app/pages/add-new-book/add-new-book.ts
+++ books-project/src/app/pages/add-new-book/add-new-book.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { BookService } from '../../core/services/book.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-new-book',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-new-book.html',
-  styleUrl: './add-new-book.css'
+  styleUrl: './add-new-book.css',
 })
 export class AddNewBook {
+  constructor(private bookService: BookService) {}
 
+  book: Book = {
+    id: '',
+    title: '',
+    description: '',
+    author: '',
+    genre: '',
+    publicationYear: 0,
+    pages: 0,
+    isbn: '',
+    image: '',
+  };
+
+  onSubmit() {
+    const accessToken = localStorage.getItem('accessToken') || '';
+
+    this.bookService.createBook(this.book, accessToken).subscribe({
+      next: (response) => {
+        console.log('Book created successfully', response);
+      },
+      error: (err) => {
+        console.error('Error creating book', err);
+      },
+    });
+  }
 }
