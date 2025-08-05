404e98a5b3b58be2553d013a753f33d6149ef7d9
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-06 00:36:36 +0300

    condtional render buttons and add comment section at book details page

--- books-project/src/app/pages/book-details-page/book-details-page.ts
+++ books-project/src/app/pages/book-details-page/book-details-page.ts
@@ -1,12 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import {
+//   CheckBookOwner,
+//   GetBookService,
+// } from '../../core/services/book.service';
+// import { Book } from '../../models/book.model';
+// import { ChangeDetectorRef } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-book-details-page',
+//   imports: [CommonModule],
+//   templateUrl: './book-details-page.html',
+//   styleUrl: './book-details-page.css',
+// })
+// export class BookDetailsPage implements OnInit {
+//   bookId!: string;
+//   currentBook!: Book;
+
+//   isOwner: boolean = false;
+//   currentUserId: string | null = null;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private getBookServ: GetBookService,
+//     private cdr: ChangeDetectorRef,
+//     private checkBookOwner: CheckBookOwner,
+//     private cd: ChangeDetectorRef
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.currentUserId = localStorage.getItem('userId');
+    
+//     this.route.paramMap.subscribe((params) => {
+//       this.bookId = params.get('id')!;
+      
+//       // get current book data
+//       this.getBookServ.getBook(this.bookId).subscribe({
+//         next: (book) => {
+//           console.log('Book data:', book);
+//           this.currentBook = book;
+//           this.cdr.detectChanges();
+//         },
+//         error: (err) => {
+//           console.log('error fetching book');
+//         },
+//       });
+//       console.log('Book ID from URL:', this.bookId);
+
+//       // check if is owner
+//       if (this.currentUserId && this.bookId) {
+//         this.checkBookOwner.getBookByOwnerAndId(this.currentUserId, this.bookId).subscribe({
+//           next: (books) => {
+//             this.isOwner = books.length > 0;
+//             console.log('Books found for owner:', books);
+//             console.log('isOwner:', this.isOwner);
+//             this.cdr.detectChanges();
+//           },
+//           error: (err) => {
+//             console.error('Error checking owner:', err);
+//           }
+//         })
+      
+//     });
+//   }
+// }
+
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GetBookService } from '../../core/services/book.service';
+import { CheckBookOwner, GetBookService } from '../../core/services/book.service';
 import { Book } from '../../models/book.model';
-import { ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-details-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.css',
 })
@@ -14,15 +82,23 @@ export class BookDetailsPage implements OnInit {
   bookId!: string;
   currentBook!: Book;
 
+  isOwner: boolean = false;
+  currentUserId: string | null = null;
+
   constructor(
     private route: ActivatedRoute,
     private getBookServ: GetBookService,
+    private checkBookOwner: CheckBookOwner,
     private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
+    this.currentUserId = localStorage.getItem('userId');
+    
     this.route.paramMap.subscribe((params) => {
       this.bookId = params.get('id')!;
+      
+      // get current book data
       this.getBookServ.getBook(this.bookId).subscribe({
         next: (book) => {
           console.log('Book data:', book);
@@ -33,7 +109,21 @@ export class BookDetailsPage implements OnInit {
           console.log('error fetching book');
         },
       });
-      console.log('Book ID from URL:', this.bookId);
+
+      // check if is owner
+      if (this.currentUserId && this.bookId) {
+        this.checkBookOwner.getBookByOwnerAndId(this.currentUserId, this.bookId).subscribe({
+          next: (books) => {
+            this.isOwner = books.length > 0;
+            console.log('Books found for owner:', books);
+            console.log('isOwner:', this.isOwner);
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error checking owner:', err);
+          }
+        });
+      }
     });
   }
 }
