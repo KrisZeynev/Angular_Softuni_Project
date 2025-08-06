c4ce25c03796de587701b9144a60a51395a938f6
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-06 17:24:09 +0300

    implement add to favorites functionality into main details page

--- books-project/src/app/pages/book-details-page/book-details-page.ts
+++ books-project/src/app/pages/book-details-page/book-details-page.ts
@@ -31,10 +31,10 @@
 
 //   ngOnInit(): void {
 //     this.currentUserId = localStorage.getItem('userId');
-    
+
 //     this.route.paramMap.subscribe((params) => {
 //       this.bookId = params.get('id')!;
-      
+
 //       // get current book data
 //       this.getBookServ.getBook(this.bookId).subscribe({
 //         next: (book) => {
@@ -61,23 +61,27 @@
 //             console.error('Error checking owner:', err);
 //           }
 //         })
-      
+
 //     });
 //   }
 // }
 
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CheckBookOwner, GetBookService } from '../../core/services/book.service';
+import {
+  CheckBookOwner,
+  GetBookService,
+} from '../../core/services/book.service';
 import { Book } from '../../models/book.model';
 import { CommonModule } from '@angular/common';
+import { FavoritesService } from '../../core/services/favorites.service';
 
 @Component({
   selector: 'app-book-details-page',
   imports: [CommonModule],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.css',
-  standalone: true
+  standalone: true,
 })
 export class BookDetailsPage implements OnInit {
   bookId!: string;
@@ -86,20 +90,22 @@ export class BookDetailsPage implements OnInit {
   isOwner: boolean = false;
   currentUserId: string | null = null;
   isFavorite: boolean = false;
+  favoriteId: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private getBookServ: GetBookService,
     private checkBookOwner: CheckBookOwner,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private favoritesService: FavoritesService
   ) {}
 
   ngOnInit(): void {
     this.currentUserId = localStorage.getItem('userId');
-    
+
     this.route.paramMap.subscribe((params) => {
       this.bookId = params.get('id')!;
-      
+
       // get current book data
       this.getBookServ.getBook(this.bookId).subscribe({
         next: (book) => {
@@ -108,28 +114,101 @@ export class BookDetailsPage implements OnInit {
           this.cdr.detectChanges();
         },
         error: (err) => {
-          console.log('error fetching book');
+          console.log('error fetching book', err);
         },
       });
 
       // check if is owner
       if (this.currentUserId && this.bookId) {
-        this.checkBookOwner.getBookByOwnerAndId(this.currentUserId, this.bookId).subscribe({
-          next: (books) => {
-            this.isOwner = books.length > 0;
-            console.log('Books found for owner:', books);
-            console.log('isOwner:', this.isOwner);
-            this.cdr.detectChanges();
-          },
-          error: (err) => {
-            console.error('Error checking owner:', err);
-          }
-        });
+        this.checkBookOwner
+          .getBookByOwnerAndId(this.currentUserId, this.bookId)
+          .subscribe({
+            next: (books) => {
+              this.isOwner = books.length > 0;
+              console.log('isOwner:', this.isOwner);
+              this.cdr.detectChanges();
+            },
+            error: (err) => {
+              console.error('Error checking owner:', err);
+            },
+          });
+
+        // Check if book is in favorites
+        this.favoritesService
+          .getFavoriteByUserAndBook(this.currentUserId, this.bookId)
+          .subscribe({
+            next: (favorites) => {
+              if (favorites.length > 0) {
+                this.isFavorite = true;
+                this.favoriteId = favorites[0]._id;
+              } else {
+                this.isFavorite = false;
+                this.favoriteId = '';
+              }
+              this.cdr.detectChanges();
+            },
+            error: (err) => {
+              console.error('Error checking favorites:', err);
+            },
+          });
       }
     });
   }
 
   addToFavorites(): void {
-    console.log('add to favorites')
+    // console.log(`Book "${this.book.title}" added to favorites.`);
+    // console.log(`Book favorites id ${this.favoriteId}`);
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      console.error('User is not authenticated!');
+      return;
+    }
+
+    console.log(`isFavorite: ${this.isFavorite}`);
+
+    if (!this.isFavorite) {
+      this.favoritesService
+        .addToFavorites(this.currentBook._id, accessToken)
+        .subscribe({
+          next: (res) => {
+            this.isFavorite = true;
+            this.favoriteId = res._id;
+            console.log(
+              `Book "${this.currentBook.title}" added to favorites.`,
+              res
+            );
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error adding to favorites:', err);
+          },
+        });
+    } else {
+      this.favoritesService
+        .removeFromFavorites(this.favoriteId, accessToken)
+        .subscribe({
+          next: (res) => {
+            this.isFavorite = false;
+            this.favoriteId = '';
+            console.log(
+              `Book "${this.currentBook.title}" removed from favorites.`,
+              res
+            );
+            // this.favoriteRemoved.emit(this.currentBook._id);
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error removing from favorites:', err);
+          },
+        });
+    }
   }
+  // addToFavorites(): void {
+  //   console.log('add to favorites ')
+  //   console.log(`isOwner: ${this.isOwner}`)
+  //   console.log(`currentUserId: ${this.currentUserId}`)
+  //   console.log(`isFavorite: ${this.isFavorite}`)
+  //   console.log(`favoriteId: ${this.favoriteId}`)
+  // }
 }
