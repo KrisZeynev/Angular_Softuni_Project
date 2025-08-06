84f83da0718f42b943a93f5a6c198860334083ac
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-06 13:36:21 +0300

    implement add and remove from favorites logic

--- books-project/src/app/components/book-details-card/book-details-card.ts
+++ books-project/src/app/components/book-details-card/book-details-card.ts
@@ -3,6 +3,7 @@ import { Book } from '../../models/book.model';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CheckBookOwner } from '../../core/services/book.service';
+import { FavoritesService } from '../../core/services/favorites.service';
 
 @Component({
   selector: 'app-book-details-card',
@@ -16,34 +17,107 @@ export class BookDetailsCard implements OnInit {
 
   isOwner: boolean = false;
   currentUserId: string | null = null;
+  isFavorite: boolean = false;
+  favoriteId: string = '';
+
+  private apiUrl = 'http://localhost:3030/data/favorites';
 
   constructor(
     private checkBookOwner: CheckBookOwner,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private favoritesService: FavoritesService,
   ) {}
 
   ngOnInit(): void {
     this.currentUserId = localStorage.getItem('userId');
 
     if (this.currentUserId && this.book && this.book._id) {
-      this.checkBookOwner.getBookByOwnerAndId(this.currentUserId, this.book._id).subscribe({
-        next: (books) => {
-          this.isOwner = books.length > 0;
-          this.cd.detectChanges();
-          console.log('Book:', books);
-          console.log('isOwner:', this.isOwner);
-        },
-        error: (err) => {
-          console.error('Error:', err);
-        }
-      });
+      this.checkBookOwner
+        .getBookByOwnerAndId(this.currentUserId, this.book._id)
+        .subscribe({
+          next: (books) => {
+            this.isOwner = books.length > 0;
+            this.cd.detectChanges();
+            // console.log('Book:', books);
+            // console.log('isOwner:', this.isOwner);
+            // console.log('isFavorite:', this.isFavorite);
+          },
+          error: (err) => {
+            console.error('Error:', err);
+          },
+        });
+
+      this.favoritesService
+        .getFavoriteByUserAndBook(this.currentUserId, this.book._id)
+        .subscribe({
+          next: (favorites) => {
+            this.isFavorite = favorites.length > 0;
+            this.favoriteId = favorites.length > 0 ? favorites[0]._id : '';
+
+            // console.log(`Book favorites id ${this.favoriteId}`);
+            // console.log('isFavorite:', this.isFavorite);
+
+            this.cd.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error fetching favorites:', err);
+          },
+        });
     } else {
       console.warn('Missing currentUserId or book._id');
     }
+
+    // console.log(`Book favorites id ${this.favoriteId}`);
+    // console.log('isOwner:', this.isOwner);
+    // console.log('isFavorite:', this.isFavorite);
   }
 
   addToFavorites(): void {
-    console.log(`Book "${this.book.title}" added to favorites.`);
+    // console.log(`Book "${this.book.title}" added to favorites.`);
+    // console.log(`Book favorites id ${this.favoriteId}`);
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      console.error('User is not authenticated!');
+      return;
+    }
+
+    console.log(`isFavorite: ${this.isFavorite}`);
+
+    if (!this.isFavorite) {
+
+      this.favoritesService
+        .addToFavorites(this.book._id, accessToken)
+        .subscribe({
+          next: (res) => {
+            this.isFavorite = true;
+            this.favoriteId = res._id; 
+            console.log(`Book "${this.book.title}" added to favorites.`, res);
+            this.cd.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error adding to favorites:', err);
+          },
+        });
+    } else {
+
+      this.favoritesService
+        .removeFromFavorites(this.favoriteId, accessToken)
+        .subscribe({
+          next: (res) => {
+            this.isFavorite = false;
+            this.favoriteId = '';
+            console.log(
+              `Book "${this.book.title}" removed from favorites.`,
+              res
+            );
+            this.cd.detectChanges();
+          },
+          error: (err) => {
+            console.error('Error removing from favorites:', err);
+          },
+        });
+    }
   }
 
   editBook(): void {
--- books-project/src/app/core/services/favorites.service.ts
+++ books-project/src/app/core/services/favorites.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavoritesService {
   private apiUrl = 'http://localhost:3030/data/favorites';
@@ -13,11 +13,27 @@ export class FavoritesService {
   addToFavorites(bookId: string, accessToken: string): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'X-Authorization': accessToken
+      'X-Authorization': accessToken,
     });
 
     const body = { bookId };
 
     return this.http.post(this.apiUrl, body, { headers });
+  }
+
+  getFavoriteByUserAndBook(userId: string, bookId: string): Observable<any[]> {
+    const query = encodeURIComponent(
+      `_ownerId="${userId}" and bookId="${bookId}"`
+    );
+    const url = `${this.apiUrl}?where=${query}`;
+    return this.http.get<any[]>(url);
+  }
+
+    removeFromFavorites(favoriteId: string, accessToken: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'X-Authorization': accessToken
+    });
+
+    return this.http.delete(`${this.apiUrl}/${favoriteId}`, { headers });
   }
 }
