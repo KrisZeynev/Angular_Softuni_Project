51e56573d90ec971ece5a50e484d5d6f5d346fdf
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-08 22:29:30 +0300

    create comment service
    create comment service for CRUD operations

---
+++ books-project/src/app/core/services/comment.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comment } from '../../models/comment.model';
+
+const baseUrl = 'http://localhost:3030/data/comments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  constructor(private http: HttpClient) {}
+
+  getCommentsByBookId(bookId: string): Observable<Comment[]> {
+    const query = encodeURIComponent(`bookId="${bookId}"`);
+    return this.http.get<Comment[]>(`${baseUrl}?where=${query}`);
+  }
+
+  postComment(bookId: string, text: string): Observable<Comment> {
+    return this.http.post<Comment>(baseUrl, { bookId, text });
+  }
+
+  editComment(commentId: string, newText: string): Observable<Comment> {
+    return this.http.patch<Comment>(`${baseUrl}/${commentId}`, { text: newText });
+  }
+
+  deleteComment(commentId: string): Observable<void> {
+    return this.http.delete<void>(`${baseUrl}/${commentId}`);
+  }
+}
