9b77ca0b28adfc4d8e78086627fcbba9c7ab5fc2
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-09 16:24:50 +0300

    implement conditional rendering for edit and delete buttons

--- books-project/src/app/components/comments/comment-item/comment-item.ts
+++ books-project/src/app/components/comments/comment-item/comment-item.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Comment } from '../../../models/comment.model';
 
 @Component({
@@ -9,6 +9,14 @@ import { Comment } from '../../../models/comment.model';
   styleUrl: './comment-item.css',
   standalone: true
 })
-export class CommentItem {
+export class CommentItem implements OnInit {
   @Input() comment!: Comment;
+  currentUserId: string | null = null;
+  isOwner: boolean = false;
+
+  ngOnInit(): void {
+    this.currentUserId = localStorage.getItem('userId');
+    this.isOwner = this.currentUserId === this.comment._ownerId ? true : false
+  }
+  
 }
