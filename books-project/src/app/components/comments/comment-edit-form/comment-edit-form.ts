580e3449ffaddbe99c0d022695010c14e139cdfa
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-09 23:53:33 +0300

    add html and style for edit comment component

--- books-project/src/app/components/comments/comment-edit-form/comment-edit-form.ts
+++ books-project/src/app/components/comments/comment-edit-form/comment-edit-form.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommentService } from '../../../core/services/comment.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comment-edit-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './comment-edit-form.html',
-  styleUrl: './comment-edit-form.css'
+  styleUrl: './comment-edit-form.css',
 })
-export class CommentEditForm {
+export class CommentEditForm implements OnInit {
+  commentForm: FormGroup;
+  bookId!: string;
+  currAccessToken: string | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private commentService: CommentService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
+    this.commentForm = this.fb.group({
+      text: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50),
+          Validators.pattern(/\S+/),
+        ],
+      ],
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    
+    // if (id) {
+    //   this.commentService.(id).subscribe(comment => {
+    //     this.commentText = comment.text;
+    //   });
+    // }
+  }
+
+  onSubmit(): void {
+    console.log('on submit')
+  }
 }
