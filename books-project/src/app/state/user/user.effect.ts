0ffca68c2c07335177fa6f8b8d1bc9a8520a3979
Kris Zeynev <kriszeynev@yahoo.com>
2025-08-11 23:26:47 +0300

    add register effect

--- books-project/src/app/state/user/user.effect.ts
+++ books-project/src/app/state/user/user.effect.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from '../../core/services/auth.service';
-import { loadUser, loadUserSuccess, loadUserFailure } from './user.actions';
+import {
+  loadUser,
+  loadUserSuccess,
+  loadUserFailure,
+  registerUser,
+  registerUserSuccess,
+  registerUserFailure,
+} from './user.actions';
 import { catchError, map, mergeMap, of } from 'rxjs';
 
 @Injectable()
@@ -13,6 +20,18 @@ export class UserEffects {
         this.authService.currentUser().pipe(
           map((user) => loadUserSuccess({ user })),
           catchError((error) => of(loadUserFailure({ error })))
+        )
+      )
+    )
+  );
+
+  registerUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(registerUser),
+      mergeMap(({ username, email, password, profileImg }) =>
+        this.authService.register(username, email, password, profileImg).pipe(
+          map((user) => registerUserSuccess({ user })),
+          catchError((error) => of(registerUserFailure({ error })))
         )
       )
     )
